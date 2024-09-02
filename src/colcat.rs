68e10fea3b71df5aaeaf6d67354c9a1638156712
Richard Osterloh <r.osterloh@gardin.co.uk>
2024-09-02 17:12:20 +0100

    Basic working version

--- src/colcat.rs
+++ src/colcat.rs
@@ -1,11 +1,43 @@
-// use anyhow::Result;
 use core::panic;
-// use core::result::Result;
 use std::{
-    env, io::Read, io::Result, io::Write, os::unix::net::UnixStream, path::Path, time::Duration,
+    env, io::BufRead, io::BufReader, io::Result, io::Write, os::unix::net::UnixStream, path::Path,
+    time::Duration,
 };
 
 static DEFAULT_SOCKET_PATH: &'static str = "/var/run/collectd-unixsock";
+
+fn read_from_socket(reader: &mut BufReader<UnixStream>) -> Vec<String> {
+    let mut results: Vec<String> = vec![];
+
+    let mut response = String::new();
+    let count = reader.read_line(&mut response).unwrap();
+    if count > 0 {
+        let mut num_values: u32 = 0;
+        if let Some((first, _)) = response.split_once(" ") {
+            num_values = first.parse().unwrap();
+        }
+
+        for _ in 0..num_values {
+            let mut response = String::new();
+            reader.read_line(&mut response).unwrap();
+            let values: Vec<&str> = response.split_whitespace().collect();
+            if values.len() == 1 {
+                // Result of GETVAL
+                results.push(values[0].to_string());
+            } else {
+                // Result of LISTVAL
+                let mut tmp = values[1].to_string();
+                if tmp.ends_with("\n") {
+                    tmp.pop();
+                }
+                results.push(tmp);
+            }
+        }
+    }
+
+    // TODO: check len results == num_values
+    results
+}
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -36,14 +68,27 @@ fn main() -> Result<()> {
     println!("Requsting list of all values");
     stream.write_all(b"LISTVAL\n")?;
 
-    //let mut response = String::new();
-    let mut response: Vec<u8> = vec![0; 10];
-    //stream.read_to_string(&mut response)?;
-    // stream.read_to_end(&mut response)?;
-    stream.read_exact(&mut response)?;
+    // let mut response: Vec<u8> = vec![0; 10];
+    // stream.read_exact(&mut response)?;
+    // let response_string = String::from_utf8_lossy(&response).to_string();
+    // println!("{response_string}");
 
-    let response_string = String::from_utf8_lossy(&response).to_string();
-    println!("{response_string}");
+    // let mut response = String::new();
+    let mut reader = BufReader::new(stream.try_clone().unwrap());
+
+    let results = read_from_socket(&mut reader);
+    // println!("{:?}", results);
+    if results.len() > 0 {
+        for metric in results {
+            let get_val = format!("GETVAL {metric}\n");
+            stream.write_all(get_val.as_bytes())?;
+
+            let metric_data = read_from_socket(&mut reader);
+            println!("{metric}: {:?}", metric_data);
+        }
+    } else {
+        println!("No metrics found");
+    }
 
     Ok(())
 }
